31df7c01d55ff23d6a3a6b1b6057fd22e96620cc
Adrian Silva <adrianfsf10@gmail.com>
2023-03-31 13:16:36 -0300

    update env

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production'
+      PORT: string
       PUBLIC_KEY: string
       PRIVATE_KEY: string
     }
